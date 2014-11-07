316922e6c84fba81188f64466e62f1f597621004
Rob Dodson <robdodson@google.com>
2014-11-06 19:10:16 -0800

    lots of css rework and add-page.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,44 @@
 // Install Service Worker
-// navigator.serviceWorker.register('/scripts/worker.js').then(function(reg) {
-//   console.log('◕‿◕', reg);
-// }, function(err) {
-//   console.log('ಠ_ಠ', err);
-// });
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/scripts/worker.js').then(function(reg) {
+    console.log('◕‿◕', reg);
+  }, function(err) {
+    console.log('ಠ_ಠ', err);
+  });
+}
 
 // Select auto-binding template and use as the top level of our app
-var app = document.querySelector('template');
+var app = document.querySelector('#app');
 app.addEventListener('template-bound', function() {
+  var pages = document.querySelector('#pages');
 
-  this.init = function() {
-    var pages = document.querySelector('#pages');
-
-    // Setup routing
-    var contacts = function() {
-      pages.selected = 0;
-    };
-    var info = function(contactId) {
-      pages.selected = 1;
-      // hacky async workaround for when the properties change
-      setTimeout(function() {
-        pages.selectedItem.querySelector('.page').contactId = contactId;
-      }, 0);
-    };
-    var add = function() {
-      pages.selected = 2;
-    };
-
-    var routes = {
-      '/': contacts,
-      'contacts/:id': info,
-      'add': add
-    };
-
-    var router = Router(routes);
-    router.init('/');
-
-    // Handle page transitions
-    pages.addEventListener('core-animated-pages-transition-prepare', function(e) {
-      pages.selectedItem.querySelector('.page').willPrepare();
-    });
+  // Setup routing
+  var contacts = function() {
+    pages.selected = 0;
   };
+  var info = function(contactId) {
+    pages.selected = 1;
+    // hacky async workaround for when the properties change
+    setTimeout(function() {
+      pages.selectedItem.querySelector('.page').contactId = contactId;
+    }, 0);
+  };
+  var add = function() {
+    pages.selected = 2;
+  };
+
+  var routes = {
+    '/': contacts,
+    'contacts/:id': info,
+    'add': add
+  };
+
+  var router = Router(routes);
+  router.init('/');
+
+  // Handle page transitions
+  pages.addEventListener('core-animated-pages-transition-prepare', function(e) {
+    pages.selectedItem.querySelector('.page').willPrepare();
+  });
 
 });
