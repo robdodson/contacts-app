3a81339ac98aa67928f30e4515c9829ce5d65142
Rob Dodson <robdodson@google.com>
2014-11-08 17:11:50 -0800

    Add to homescreen

--- Gruntfile.js
+++ Gruntfile.js
@@ -233,7 +233,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,txt}',
+            '*.{ico,txt,png}',
             '.htaccess',
             '*.html',
             'elements/**',
